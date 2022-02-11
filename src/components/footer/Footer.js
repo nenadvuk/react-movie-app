9be3cd0f14470aef2479bf206d1c8568a31dc4ea
nenad_vuk <nenadvukasinovic@rs.gmail.com>
2022-02-11 23:03:19 +0100

    final commit

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -11,7 +11,7 @@ const Footer = () => {
         <div className="footer__content__logo">
           <div className="logo">
             <img src={logo} alt="" />
-            <Link to="/">Wikimovies</Link>
+            <Link to="/">Movizilla</Link>
           </div>
         </div>
         <div className="footer__content__menus">
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -46,7 +46,7 @@ const Header = () => {
       <div className="header__wrap container">
         <div className="logo">
           <img src={logo} alt="" />
-          <Link to="/">Wikimovies</Link>
+          <Link to="/">Movizilla</Link>
         </div>
         <ul className="header__nav">
           {headerNav.map((e, i) => (
